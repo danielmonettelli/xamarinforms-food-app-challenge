25d21fdeffa0d81e1548b99f0faa46562271383f
Daniel A. Monetelli L <danielmonetelli@hotmail.com>
2019-09-05 21:43:32 -0500

    Add project files.

---
+++ XF_FoodApp/XF_FoodApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XF_FoodApp.Models;
+
+namespace XF_FoodApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
